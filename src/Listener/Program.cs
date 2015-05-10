4e8f33824cbf5f211e766f124216981d51767488
Alberto Monteiro <alberto.monteiro@live.com>
2015-05-09 21:42:34 -0300

    first commit

---
+++ src/Listener/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Rebus.Configuration;
+using Rebus.Logging;
+using Rebus.Transports.Msmq;
+
+namespace Listener
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // we have the container in a variable, but you would probably stash it in a static field somewhere
+            using (var adapter = new BuiltinContainerAdapter())
+            {
+                Configure.With(adapter)
+                    .Logging(t => t.None())
+                    .Transport(t => t.UseMsmqAndGetInputQueueNameFromAppConfig())
+                    .MessageOwnership(d => d.FromRebusConfigurationSection())
+                    .CreateBus().Start();
+
+                adapter.Handle<DateTime>(currentDateTime => Console.WriteLine("The time is {0}", currentDateTime));
+                
+                Console.WriteLine("Press enter to quit");
+                Console.ReadLine();
+            } //< always dispose bus when your app quits - here done via the container adapter
+        }
+    }
+}
---
+++ src/Sender/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Timers;
+using Rebus.Configuration;
+using Rebus.Logging;
+using Rebus.Transports.Msmq;
+
+namespace Sender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var adapter = new BuiltinContainerAdapter())
+            {
+                Configure.With(adapter)
+                    .Logging(t => t.None())
+                    .Transport(t => t.UseMsmqInOneWayClientMode())
+                    .MessageOwnership(d => d.FromRebusConfigurationSection())
+                    .CreateBus().Start();
+
+                var timer = new Timer();
+                timer.Elapsed += (o, a) =>
+                {
+                    var dateTime = DateTime.Now;
+                    Console.WriteLine("Enviando {0}", dateTime);
+                    adapter.Bus.Send(dateTime);
+                };
+                timer.Interval = 1000;
+                timer.Start();
+
+                Console.WriteLine("Press enter to quit");
+                Console.ReadLine();
+            } //< always dispose bus when your app quits - here done via the container adapter
+
+        }
+    }
+}
